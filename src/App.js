df9ca2a1bdb2045d126ce09e15e511953394f4c8
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-07 18:58:19 +0200

    Set up homepage background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import CoffeeHub from "./containers/CoffeeHub/CoffeeHub";
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<CoffeeHub />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/containers/CoffeeHub/CoffeeHub.js
@@ -0,0 +1,10 @@
+import HomePage from "../../components/HomePage/HomePage";
+import "./CoffeeHub.css";
+
+export default function CoffeeHub() {
+	return (
+		<div className="CoffeeHub">
+			<HomePage />
+		</div>
+	);
+}
