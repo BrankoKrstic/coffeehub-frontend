4b85f29a31606cb52d62db95431934d88468e54a
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-08 00:38:39 +0200

    Set up homepage design

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,17 @@
+import LinkPrimary from "../Navigation/LinkPrimary/LinkPrimary";
 import "./HomePage.css";
 
 export default function HomePage() {
-	return <div className="HomePage"></div>;
+	return (
+		<div className="HomePage">
+			<div className="HomePage-titlecontainer">
+				<p>Welcome to</p>
+				<h1>CoffeeHub</h1>
+			</div>
+			<div className="HomePage-linkcontainer">
+				<LinkPrimary to="#">Start Shopping</LinkPrimary>
+				<LinkPrimary to="#">Learn Our Story</LinkPrimary>
+			</div>
+		</div>
+	);
 }
--- src/containers/CoffeeHub/CoffeeHub.js
+++ src/containers/CoffeeHub/CoffeeHub.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import HomePage from "../../components/HomePage/HomePage";
 import "./CoffeeHub.css";
 
 export default function CoffeeHub() {
 	return (
 		<div className="CoffeeHub">
-			<HomePage />
+			<Router>
+				<HomePage />
+			</Router>
 		</div>
 	);
 }
