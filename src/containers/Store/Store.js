7750f5f42c089b04cc47aac6ef6859d92f4b1ce2
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-08 16:22:15 +0200

    fetch product data from the backend API

--- src/containers/Store/Store.js
+++ src/containers/Store/Store.js
@@ -1,30 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "../../axios-orders";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import "./Store.css";
 
 export default function Store() {
-	const products = [
-		{
-			name: "Coffee Bag",
-			categories: ["coffee"],
-			price: 0.99,
-			id: 1,
-			image: "https://images.unsplash.com/photo-1605711599412-775918dbe770?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1722&q=80",
-		},
-		{
-			name: "Coffee Bag2",
-			categories: ["coffee"],
-			price: 0.99,
-			id: 2,
-			image: "https://images.unsplash.com/photo-1605711599412-775918dbe770?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1722&q=80",
-		},
-		{
-			name: "Coffee Bag3",
-			categories: ["coffee"],
-			price: 0.99,
-			id: 3,
-			image: "https://images.unsplash.com/photo-1605711599412-775918dbe770?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1722&q=80",
-		},
-	];
+	const [productState, setProductState] = useState([]);
+	useEffect(() => {
+		axios.get("/products").then((res) => {
+			setProductState(res.data.products);
+		});
+	}, []);
 	return (
 		<div className="Store">
 			<h1>Our Coffees</h1>
@@ -38,7 +23,7 @@ export default function Store() {
 					<button className="Store-filter-button">Gear</button>
 				</div>
 				<div className="Store-products">
-					{products.map((product, i) => (
+					{productState.map((product, i) => (
 						<ProductCard key={i} {...product} />
 					))}
 				</div>
