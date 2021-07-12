d4f2fd818f0ef5ab36db7eaa618a29ffe9f376a1
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-12 16:16:19 +0200

    Style edits

--- src/containers/NewProduct/NewProduct.js
+++ src/containers/NewProduct/NewProduct.js
@@ -1,28 +1,53 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import axios from "../../axios-orders";
 import "./NewProduct.css";
 
 export default function NewProduct() {
+	const history = useHistory();
+	const [productState, setProductState] = useState({
+		name: "",
+		categories: "",
+		price: 0,
+		image: "",
+		description: "",
+	});
+	const handleChange = (e) => {
+		setProductState({ ...productState, [e.target.name]: e.target.value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const product = productState;
+		product.categories = product.categories.split(" ");
+		axios
+			.post("/products", { product: product })
+			.then((res) => history.push("/store"))
+			.catch((err) => console.log(err));
+	};
 	return (
 		<div className="NewProduct">
-			<form className="NewProduct-form">
-				<label for="name">Name:</label>
+			<form className="NewProduct-form" onSubmit={handleSubmit}>
+				<label htmlFor="name">Name:</label>
 				<Input
 					type="text"
 					id="name"
 					name="name"
 					placeholder="Product name"
+					updateForm={handleChange}
 					required
 				/>
-				<label for="categories">Categories:</label>
+				<label htmlFor="categories">Categories:</label>
 				<Input
 					type="text"
 					id="categories"
 					name="categories"
 					placeholder="Product categories"
+					updateForm={handleChange}
 					required
 				/>
-				<label for="price">Price:</label>
+				<label htmlFor="price">Price:</label>
 				<Input
 					type="number"
 					id="price"
@@ -30,22 +55,25 @@ export default function NewProduct() {
 					placeholder="Product price"
 					step={0.01}
 					min={0.01}
+					updateForm={handleChange}
 					required
 				/>
-				<label for="image">Image:</label>
+				<label htmlFor="image">Image:</label>
 				<Input
 					type="text"
 					id="image"
 					name="image"
 					placeholder="Image link"
+					updateForm={handleChange}
 					required
 				/>
-				<label for="description">Description:</label>
+				<label htmlFor="description">Description:</label>
 				<textarea
 					type="text"
 					id="description"
 					name="description"
 					placeholder="Product description"
+					onChange={handleChange}
 					required
 				></textarea>
 				<Button secondary type="submit">
