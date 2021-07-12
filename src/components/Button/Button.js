677f17ec0951656ee675220018f7d45ecf81575f
BrankoKrstic <brankokrstic94@gmail.com>
2021-07-12 17:41:34 +0200

    add store filters

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,9 @@ export default function Button(props) {
 		<button
 			className={`Button ${props.primary && "Primary"} ${
 				props.secondary && "Secondary"
-			} ${props.remove && "Remove"}`}
+			} ${props.remove && "Remove"} ${props.filter && "Filter"} ${
+				props.active && "Filter-active"
+			}`}
 			onClick={props.clicked}
 			type={props.type}
 		>
--- src/containers/Store/Store.js
+++ src/containers/Store/Store.js
@@ -1,14 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { initProducts } from "../../store/actions/products";
 import { cartAdd, cartIncreaseQty } from "../../store/actions/cart";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import Button from "../../components/Button/Button";
 import Loader from "../../components/UI/Loader/Loader";
 import "./Store.css";
 
 export default function Store() {
+	const [filter, setFilter] = useState(null);
 	const { products, error, loading } = useSelector((state) => state.product);
 	const { cartItems } = useSelector((state) => state.cart);
+	const filterData = [
+		{
+			value: "coffee",
+			text: "Coffee",
+		},
+		{
+			value: "coldbrew",
+			text: "Cold Brew",
+		},
+		{
+			value: "gear",
+			text: "Gear",
+		},
+	];
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch(initProducts());
@@ -20,20 +36,37 @@ export default function Store() {
 			dispatch(cartAdd(product, qty));
 		}
 	};
-	const productItems = products.map((product, i) => (
-		<ProductCard key={i} {...product} addItem={() => addItem(product, 1)} />
-	));
+	const productItems = products.map((product, i) => {
+		if (filter && !product.categories.includes(filter)) {
+			return undefined;
+		} else {
+			return (
+				<ProductCard
+					key={i}
+					{...product}
+					addItem={() => addItem(product, 1)}
+				/>
+			);
+		}
+	});
 	return (
 		<div className="Store">
 			<h1>Our Coffees</h1>
 			<div className="Store-main">
 				<div className="Store-filters">
 					<h3>Filters</h3>
-					<button className="Store-filter-button Store-filter-active">
-						Coffee
-					</button>
-					<button className="Store-filter-button">Cold Brew</button>
-					<button className="Store-filter-button">Gear</button>
+					{filterData.map((btn) => (
+						<Button
+							filter
+							active={filter === btn.value}
+							clicked={() => setFilter(btn.value)}
+						>
+							{btn.text}
+						</Button>
+					))}
+					<Button filter clicked={() => setFilter(null)}>
+						Clear filter
+					</Button>
 				</div>
 				<div className="Store-products">
 					{error ? (
